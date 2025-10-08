fe0857ec2a1959dd989254bdc523c7bbf34e8e1a
sourcerich <richiepatilwork@gmail.com>
2025-10-09 04:29:08 +0530

    Fix profile image path - rename to lowercase extension for case-sensitive deployment

--- app/pages/about.vue
+++ app/pages/about.vue
@@ -36,7 +36,7 @@ useSeoMeta({
       <NuxtImg
         class="sm:rotate-4 size-36 rounded-lg ring ring-default ring-offset-3 ring-offset-(--ui-bg) object-cover"
         :src="global.picture?.light!"
-        :alt="global.picture?.alt!"
+        :alt="global.picture?.dark!"
         width="144"
         height="144"
         quality="95"
